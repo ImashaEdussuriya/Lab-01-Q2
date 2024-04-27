50c472e80a453f3b1c5ec827144d3bf55a94a5aa
Imasha <Imasha@SILVERBUNNY>
2024-04-28 00:00:59 +0530

    Add project files.

---
+++ Lab 01 Q2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Enter number {i + 1}:");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (IsEven(number))
+                {
+                    Console.WriteLine("Even");
+                }
+                else
+                {
+                    Console.WriteLine("Odd");
+                }
+            }
+        }
+
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
+
+
+
